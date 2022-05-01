f66a9aa50f3977eb8881a92e2d14952a8177a05e
Ashten <ashten@Ashtens-MacBook-Pro.local>
2022-04-30 23:25:07 -0700

    second commit

---
+++ deribitOptions.py
@@ -0,0 +1,73 @@
+import asyncio
+import websockets
+import json
+import pandas as pd
+
+instruments_msg = \
+{
+  "jsonrpc": "2.0",
+  "id": 7617,
+  "method": "public/get_instruments",
+  "params": {
+    "currency": "BTC",
+    "kind": "option",
+    "expired": False
+  }
+}
+
+lastTrades = \
+{
+  "jsonrpc": "2.0",
+  "id": 9267,
+  "method": "public/get_last_trades_by_instrument",
+  "params": {
+    "instrument_name": "a",
+    "count": 1000,
+    "include_old": True,
+    "sorting": "desc"
+  }
+}
+
+
+async def call_api(msg, lastTrades):
+    async with websockets.connect('wss://www.deribit.com/ws/api/v2') as websocket:
+        await websocket.send(msg)
+        response = await websocket.recv()
+        response = json.loads(response)
+
+        instruments = []
+
+        for i in range(len(response["result"])):
+           instruments.append(response["result"][i]["instrument_name"])
+
+        history = []
+        for i in range(len(instruments)):
+            a = json.loads(lastTrades)
+            a["params"]["instrument_name"] = instruments[i]
+            a = json.dumps(a)
+            await websocket.send(a)
+            response = await websocket.recv()
+            response = json.loads(response)
+            temp = []
+            temp.append(instruments[i])
+            buy = 0
+            sell = 0
+            for j in range(len(response["result"]["trades"])):
+                if response["result"]["trades"][j]["direction"] == "sell":
+                    sell += int(response["result"]["trades"][j]["amount"])
+
+                elif response["result"]["trades"][j]["direction"] == "buy":
+                    buy += int(response["result"]["trades"][j]["amount"])
+
+            temp.append(buy)
+            temp.append(sell)
+            history.append(temp)
+            print(i)
+
+        df = pd.DataFrame(history)
+        df.to_csv("deribit.csv", index=False, header=False)
+
+
+
+
+asyncio.get_event_loop().run_until_complete(call_api(json.dumps(instruments_msg), json.dumps(lastTrades)))
